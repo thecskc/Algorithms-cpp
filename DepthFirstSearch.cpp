b486b8649b611cf723024031f113c737c42c66c2
Santhoshkrishnachaitanya Chelikavada <cskc97@users.noreply.github.com>
2017-02-05 23:40:41 +0800

    Add files via upload

--- DepthFirstSearch.cpp
+++ DepthFirstSearch.cpp
@@ -8,11 +8,52 @@
 
 using namespace std;
 
-typename vector<list<int>> Graph;
+typedef vector<list<int>> Graph;
+
+//Vertex representations are in adjacency lists.
+
+//Goal node is 8 and source node is 
 
 struct VertexInfo
 {
+	int distance; //distance from source node to a particular node
+	int previous_node; //previous node to a particular node in a solution path
 
 
 
 };
+
+
+void DFS(Graph& adjacencyList, int source)
+{
+
+
+
+
+}
+
+
+
+
+
+
+int main()
+{
+	Graph adjacencyList(8);
+	adjacencyList[0].push_back(1);
+	adjacencyList[0].push_back(2);
+	adjacencyList[1].push_back(3);
+	adjacencyList[1].push_back(4);
+	adjacencyList[2].push_back(4);
+	adjacencyList[2].push_back(5);
+	adjacencyList[3].push_back(6);
+	adjacencyList[4].push_back(7);
+	adjacencyList[5].push_back(7);
+	adjacencyList[6].push_back(7);
+
+	DFS(adjacencyList, 0);
+
+
+
+
+}
