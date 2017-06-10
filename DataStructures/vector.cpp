fcc4e343a0ef2ea2f520133917f41803a633de07
Santhoshkrishnachaitanya Chelikavada <cskc97@users.noreply.github.com>
2017-06-10 20:53:17 +0530

    Add files via upload

--- DataStructures/vector.cpp
+++ DataStructures/vector.cpp
@@ -2,8 +2,22 @@
 #include<fstream>
 using namespace std;
 
+template<class Object>
 class Vector
 {
+private:
+	Object* data;
+	const int INITIAL_CAPACITY = 16;
+	
+public:
+	Vector() :data(new Object[INITIAL_CAPACITY]) {};
+
+
+	~Vector()
+	{
+		delete[] data;
+	}
+
 
 
 
