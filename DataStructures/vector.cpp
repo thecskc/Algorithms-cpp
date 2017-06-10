3b861de6c312c27abb6c45dd673f6c2f25476897
Santhoshkrishnachaitanya Chelikavada <cskc97@users.noreply.github.com>
2017-06-10 21:02:34 +0530

    Add files via upload

--- DataStructures/vector.cpp
+++ DataStructures/vector.cpp
@@ -7,16 +7,44 @@ class Vector
 {
 private:
 	Object* data;
-	const int INITIAL_CAPACITY = 16;
+	int size;
+	int capacity;
+	const int INITIAL_SIZE = 16;
+
 	
 public:
-	Vector() :data(new Object[INITIAL_CAPACITY]) {};
+
+	static const int SPARE_CAPACITY = 16;
+	Vector(int theSize=INITIAL_SIZE, int theCapacity= INITIAL_SIZE+SPARE_CAPACITY) :data(new Object[theCapacity]),
+		size(theSize),capacity(theCapacity) {};
 
 
 	~Vector()
 	{
 		delete[] data;
 	}
+
+	Vector<Object>& operator=(const Vector<Object>& rhs)
+	{
+		if (this != &rhs)
+		{
+			delete[] data;
+			data = new Object[rhs.capacity];
+			for (int counter = 0;counter < rhs.size;counter++)
+			{
+				data[counter] = rhs.data[counter];
+			}
+			size = rhs.size;
+			capacity = rhs.capacity;
+
+			
+
+		}
+		return this;
+
+
+	}
+
 
 
 
