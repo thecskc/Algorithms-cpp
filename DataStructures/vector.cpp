3b8acb8b9133b3a82b501f180bff52ab56e49cb9
Santhoshkrishnachaitanya Chelikavada <cskc97@users.noreply.github.com>
2017-06-10 21:12:30 +0530

    Add files via upload

--- DataStructures/vector.cpp
+++ DataStructures/vector.cpp
@@ -2,6 +2,8 @@
 #include<fstream>
 using namespace std;
 
+
+//The data is handled via an array that the Object pointer (termed data in private) is pointing to.
 template<class Object>
 class Vector
 {
@@ -15,15 +17,34 @@ private:
 public:
 
 	static const int SPARE_CAPACITY = 16;
+
+	//Default Constructor
 	Vector(int theSize=INITIAL_SIZE, int theCapacity= INITIAL_SIZE+SPARE_CAPACITY) :data(new Object[theCapacity]),
-		size(theSize),capacity(theCapacity) {};
+		size(theSize),capacity(theCapacity) {} 
 
-
-	~Vector()
+	//Copy Constructor
+	Vector(const Vector<Object>& rhs):data(new Object[rhs.capacity]),size(rhs.size),capacity(rhs.capacity)
 	{
-		delete[] data;
+		for (int counter = 0;counter < size;counter++)
+		{
+			data[counter] = rhs.data[counter];
+		}
+
+
 	}
 
+	//move copy constructor
+	Vector(Vector&& rhs) :size(rhs.size), capacity(rhs.capacity),data(rhs.data)
+	{
+		rhs.data = nullptr;
+		rhs.capacity = 0;
+		rhs.size = 0;
+		
+
+	}
+
+
+	//Assignment Operator
 	Vector<Object>& operator=(const Vector<Object>& rhs)
 	{
 		if (this != &rhs)
@@ -44,6 +65,21 @@ public:
 
 
 	}
+
+	//Move assignment operator
+	Vector& operator=(Vector&& rhs)
+	{
+
+	}
+
+
+	//Destructor
+	~Vector()
+	{
+		delete[] data;
+	}
+
+	
 
 
 
